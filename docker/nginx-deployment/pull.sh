8be2df76bcd733edff04d60ba8c22703ab2fa4f9
Yunus Emre Gurses <gursesyunus@hotmail.com>
2024-02-08 10:32:26 +0300

    :rocket: fix tagging tofhir-web image

--- docker/nginx-deployment/pull.sh
+++ docker/nginx-deployment/pull.sh
@@ -2,4 +2,4 @@
 
 docker login nexus.srdc.com.tr:18445 --username tofhir --password-stdin < srdc-docker-registry-password.txt
 docker pull nexus.srdc.com.tr:18445/srdc/tofhir-web:dt4h-nginx
-docker tag nexus.srdc.com.tr:18445/srdc/tofhir-web:dt4h-nginx srdc/tofhir-web:dt4h
+docker tag nexus.srdc.com.tr:18445/srdc/tofhir-web:dt4h-nginx srdc/tofhir-web:dt4h-nginx
