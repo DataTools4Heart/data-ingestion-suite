7b9879267198d3b762dd6385e589412338c2b4f4
Yunus Emre Gurses <gursesyunus@hotmail.com>
2024-02-29 15:22:00 +0300

    :rocket: remove projects.json before running with docker compose because it may cause problems

--- docker/clinical-site/run.sh
+++ docker/clinical-site/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+rm -f data-ingestion-suite/projects.json
 docker compose -f data-ingestion-suite/docker/clinical-site/docker-compose-tofhir.yml --project-directory ./ -p dt4h-tofhir up -d
--- docker/nginx-deployment/run.sh
+++ docker/nginx-deployment/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+rm -f data-ingestion-suite/projects.json
 docker compose -f data-ingestion-suite/docker/nginx-deployment/docker-compose-tofhir.yml --project-directory ./ -p dt4h-tofhir up -d
