3a8c30b6c20ad9ddf473f2ff4622d9e6e9b790bc
yemregurses <gursesyunus@hotmail.com>
2024-08-15 09:50:26 +0300

    :memo: update doc and scripts

--- docker/deployment/pull.sh
+++ docker/deployment/pull.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+echo "Pulling tofhir-server, elasticsearch, kibana, and fluentd images from public Docker Hub..."
+docker compose -f data-ingestion-suite/docker/deployment/docker-compose-tofhir.yml pull tofhir-server elasticsearch kibana fluentd
+echo "Pulling tofhir-web image from private SRDC repo..."
 docker login nexus.srdc.com.tr:18445 --username tofhir --password-stdin < srdc-docker-registry-password.txt
 docker pull nexus.srdc.com.tr:18445/srdc/tofhir-web:dt4h
 docker tag nexus.srdc.com.tr:18445/srdc/tofhir-web:dt4h srdc/tofhir-web:dt4h
