6b5cdc9aa40119f3a7f5f7e46ba02a2cb2bbea71
Dogukan Cavdaroglu <dogukan@srdc.com.tr>
2026-04-14 11:41:31 +0300

    :wrench: build/conf: Replace nexus.srdc.com.tr:18445 by docker.srdc.com.tr

--- docker/server/pull.sh
+++ docker/server/pull.sh
@@ -2,6 +2,6 @@
 echo "Pulling tofhir-server, elasticsearch, kibana, and fluentd images from public Docker Hub..."
 docker compose -f data-ingestion-suite/docker/server/docker-compose-tofhir.yml pull mongo onfhir tofhir-server elasticsearch kibana fluentd
 echo "Pulling tofhir-web image from private SRDC repo..."
-docker login nexus.srdc.com.tr:18445 --username tofhir --password-stdin < srdc-docker-registry-password.txt
-docker pull nexus.srdc.com.tr:18445/srdc/tofhir-web:dt4h
-docker tag nexus.srdc.com.tr:18445/srdc/tofhir-web:dt4h srdc/tofhir-web:dt4h
+docker login docker.srdc.com.tr --username tofhir --password-stdin < srdc-docker-registry-password.txt
+docker pull docker.srdc.com.tr/srdc/tofhir-web:dt4h
+docker tag docker.srdc.com.tr/srdc/tofhir-web:dt4h srdc/tofhir-web:dt4h
